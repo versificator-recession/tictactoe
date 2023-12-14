74100f252525a55c304944044ab623d884e30118
Neil Zimmerman <4504595@gmail.com>
2023-12-13 22:04:45 -0600

    12/13/2023 Update

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
-
 const plays = ["X", "O"];
 
 let gameActive = true;
 let winner = null;
+let winningPath = [null, null, null];
 let currentPlayer = Math.round(Math.random());
 let ticTacToeBoard = Array(9).fill(null);
 
+let currentPlayerDisplay = document.getElementById("currentPlayer");
+let displayMessage = document.getElementById("span1");
+currentPlayerDisplay.innerHTML = plays[currentPlayer];
+
+
 
 function checkWin () {
-
 	const winConditions = [
-	    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+	   [0, 1, 2], [3, 4, 5], [6, 7, 8],
 		[0, 3, 6], [1, 4, 7], [2, 5, 8],
 		[0, 4, 8], [2, 4, 6]
 	];
@@ -18,54 +22,55 @@ function checkWin () {
 	for (let i = 0; i <winConditions.length; i++) {
 		const [a, b, c] = winConditions[i];
 		if (ticTacToeBoard[a] && ticTacToeBoard[a] === ticTacToeBoard[b] && ticTacToeBoard[a] === ticTacToeBoard[c]) {
-			postGame(winner);
+			winningPath = winConditions[i].map(value => value + 1);
 			return ticTacToeBoard[a];
 		}
 	}
-
 	return null;
 }
 
 function gridAnimations () {
 	document.querySelectorAll(".ticTacToeBoard div").forEach(function(div) {
-	    div.style.textShadow = "5px 5px 5px black";
+	    div.style.backgroundColor = "lightgray";
 	});
-	document.querySelector(".box1").style.backgroundColor = "lightblue";
-	document.querySelector(".box2").style.backgroundColor = "yellow";
-	document.querySelector(".box3").style.backgroundColor = "lightgreen";
-	document.querySelector(".box5").style.backgroundColor = "rgba(255, 0, 0, 0.4";
-	document.querySelector(".box6").style.backgroundColor = "lightblue";
-	document.querySelector(".box9").style.backgroundColor = "yellow";
+	winningPath.forEach(function(number) {
+		let box = document.querySelector(".box" + number);
+		box.style.backgroundColor = "rgba(235, 255, 0, 90%)";
+		box.style.textShadow = "5px 5px 5px black";
+	});
+	winningPath.forEach(function(number, index) {
+		let box = document.querySelector(".box" + number);
+		box.style.animationDelay = `${index}.0s`;
+		box.style.animation = `slowSpin 1.5s 1 ${index - index * 0.5}s`;
+	});
 }
 
 function postGame (winner) {
 	gameActive = false;
-	document.querySelectorAll(".box1").forEach(function(div) {
-		div.style.backgroundColor = 'rgba(255, 0, 0, 0.5)';
-	});
-
+	displayMessage.innerHTML = "WINNING PLAYER";
+	currentPlayerDisplay.innerHTML = winner;
 	gridAnimations ();
-
 }
 
 document.getElementById('resetButton').addEventListener('click', function() {
     location.reload();
 });
 
-
 document.addEventListener('DOMContentLoaded', function () {
-    const squares = document.querySelectorAll('.ticTacToeBoard div');
+   const squares = document.querySelectorAll('.ticTacToeBoard div');
 
 	squares.forEach(function (square, index) {
-	    square.addEventListener('click', function () {
-	        if (gameActive && this.textContent === '') {
-	            this.textContent = plays[currentPlayer];
-	            ticTacToeBoard[index] = plays[currentPlayer];
-	            currentPlayer = currentPlayer ^ 1;
-	            winner = checkWin();
-	        }
+	   square.addEventListener('click', function () {
+	      if (gameActive && this.textContent === '') {
+	         this.textContent = plays[currentPlayer];
+	         ticTacToeBoard[index] = plays[currentPlayer];
+	         currentPlayer = currentPlayer ^ 1;
+	         currentPlayerDisplay.innerHTML = plays[currentPlayer];
+	         winner = checkWin();
+	         if (winner) { postGame(winner) };
+	      }
 
-	    });
+	   });
 
 	});
 
